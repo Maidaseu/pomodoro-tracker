e129b701b22420c712003834d1c8fc43bd9eb40c
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-04-23 09:56:00 +0100

    add imports for password reset functionality

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from database import init_db, get_db
 from helpers import login_required
+from secrets import token_urlsafe
+from datetime import datetime, timedelta, date
+import json
 
 app = Flask(__name__)
 
